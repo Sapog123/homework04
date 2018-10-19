7785699883c8cf5f8e3e98fad01d70ad717b7f08
Sapog123 <43165055+Sapog123@users.noreply.github.com>
2018-10-19 16:34:38 +0500

    Update Tree.scala

--- src/main/scala/fintech/homework04/Tree.scala
+++ src/main/scala/fintech/homework04/Tree.scala
@@ -6,16 +6,21 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
   // реализовать функцию fold
-  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = ???
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = {
+    t match {
+      case leaf: Leaf[A] => f(leaf.value)
+      case branch: Branch[A] => g(fold(branch.left)(f)(g), fold(branch.right)(f)(g))
+    }
+  }
 
   // реализовать следующие функции через fold
 
-  def size[A](t: Tree[A]): Int = ???
+  def size[A](t: Tree[A]): Int = fold(t)((_) => 1)((l,r)=>l+r)
 
-  def max(t: Tree[Int]): Int = ???
+  def max(t: Tree[Int]): Int = fold(t)((v)=>v)((l,r)=> Math.max(l,r))
 
-  def depth[A](t: Tree[A]): Int = ???
+  def depth[A](t: Tree[A]): Int = fold(t)((_)=> 1)((l,r)=>Math.max(l,r)+1)
 
   //здесь возможно придется прибегнуть к насильному указанию типа: Leaf(a): Tree[A]
-  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = ???
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(v=> Leaf(f(v)):Tree[B])((l,r)=> Branch(l,r))
 }
